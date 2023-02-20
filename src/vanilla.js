0dce58183506c5a896fe3fe0a1b4bb3ddf95f3c8
fcoverdugo <fcoverdugoa@underpost.net>
2023-02-20 12:39:32 -0300

    update

--- src/vanilla.js
+++ src/vanilla.js
@@ -8,30 +8,6 @@ const append = (el, html) => s(el).insertAdjacentHTML('beforeend', html);
 const prepend = (el, html) => s(el).insertAdjacentHTML('afterbegin', html);
 const sa = (el) => document.querySelectorAll(el); // .forEach((currentValue, currentIndex, listObj)
 
-const fadeIn = (el, display) => {
-  el.style.opacity = 0;
-  el.style.display = display || 'block';
-  const fade = () => {
-    let val = parseFloat(el.style.opacity);
-    if (!((val += 0.1) > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  };
-  fade();
-};
-const fadeOut = (el) => {
-  el.style.opacity = 1;
-  const fade = () => {
-    if ((el.style.opacity -= 0.1) < 0) {
-      el.style.display = 'none';
-    } else {
-      requestAnimationFrame(fade);
-    }
-  };
-  fade();
-};
-
 const borderChar = (px, color) => /*html*/ `
 text-shadow: ${px}px -${px}px ${px}px ${color},
                          -${px}px ${px}px ${px}px ${color},
@@ -44,13 +20,6 @@ const renderLang = (langs) => {
   return langs['en'];
 };
 // s('html').lang = 'en';
-
-const renderSpinner = (IDS, options) => /*html*/ `
-<div class='${options && options.class ? options.class : 'in container'} ${IDS ? IDS : ''}' 
-     style='${options && options.style ? options.style : 'text-align: center; display: none;'}'>
-    <div class='lds-ellipsis'><div></div><div></div><div></div><div></div></div>
-</div>
-`;
 
 const renderTable = (data, options) =>
   data[0]
@@ -106,13 +75,14 @@ const setURI = (uri, objData, title) => history.pushState(objData, title, uri);
 
 const getURI = () => location.pathname;
 
-const serviceRequest = (url, options) =>
-  new Promise((resolve, reject) => {
-    fetch(url(), options)
+const serviceRequest = (url, options) => {
+  if (typeof url === 'function') url = url();
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
       .then(async (res) => {
         let raw = await res.clone();
         raw = await raw.text();
-        if (options && options.log) console.log(`${url()} raw: `, raw);
+        if (options && options.log) console.log(`${url} raw: `, raw);
         if (options && options.raw === true) return raw;
         let returnObj;
         try {
@@ -123,95 +93,14 @@ const serviceRequest = (url, options) =>
         return { ...returnObj, codeStatus: res.status, raw };
       })
       .then((res) => {
-        if (options && options.log) console.log('fetch success', url(), res);
+        if (options && options.log) console.log('fetch success', url, res);
         resolve(res);
       })
       .catch((error) => {
-        if (options && options.log) console.error('fetch error ', url(), error);
+        if (options && options.log) console.error('fetch error ', url, error);
         reject(error);
       });
   });
-
-const renderFixModal = (options) => {
-  const timeOut = options && options.time ? options.time : 2500;
-  setTimeout(() => (s('.' + options.id) ? fadeIn(s('.' + options.id)) : null));
-  setTimeout(() => (s('.' + options.id) ? fadeOut(s('.' + options.id)) : null), timeOut);
-  setTimeout(() => {
-    if (s('.' + options.id)) s('.' + options.id).remove();
-    if (s('.style-' + options.id)) s('.style-' + options.id).remove();
-  }, timeOut + 500);
-  return /*html*/ `
-    <style class='style-${options.id}'>
-        .${options.id} {
-            width: ${options.width ? options.width : '200'}px;
-            height: ${options.height ? options.height : '120'}px;
-            display: none;
-            color: ${options.color};
-            z-index: 9999;
-            font-weight: bold;                                        
-        }
-    </style>
-    <mini-modal class='fix center ${options.id}'>
-        <div class='abs center'>
-            <span style='font-size: 35px'> ${options.icon} </span> 
-            <br> <br>
-            ${typeof options.content == 'function' ? options.content() : options.content}
-        </div>
-    </mini-modal>
-                    
-`;
-};
-
-const renderToggleSwitch = (options) => {
-  const factor = options && options.factor ? options.factor : 25;
-  const wFactor = 2.3;
-  const roundFactor = 0.7;
-  setTimeout(() => {
-    s(`.ts-container-${options.id}`).onclick = () => {
-      if (s(`.${options.id}`).checked) {
-        s(`.${options.id}`).checked = false;
-        s(`.ts-round-${options.id}`).style.left = factor * 0.1 + 'px';
-        htmls(`.ts-label-${options.id}`, options.label[0]);
-        s('.ts-round-' + options.id).style.background = 'gray';
-      } else {
-        s(`.${options.id}`).checked = true;
-        s(`.ts-round-${options.id}`).style.left = factor * wFactor - factor * 0.1 - factor * roundFactor + 'px';
-        htmls(`.ts-label-${options.id}`, options.label[1]);
-        s('.ts-round-' + options.id).style.background = 'green';
-      }
-      if (options && options.onChange) options.onChange(s(`.${options.id}`).checked);
-    };
-
-    if (options && options.checked == true) s(`.ts-container-${options.id}`).click();
-  });
-  return /*html*/ `
-    <style>
-        .ts-container-${options.id} {
-            width: ${factor * wFactor}px;
-            height: ${factor}px;
-            background: #121212;
-            border-radius: ${factor * 0.1}px;
-            cursor: pointer;
-        }
-        .ts-round-${options.id} {
-            border-radius: 50%;
-            height: ${factor * roundFactor}px;
-            width: ${factor * roundFactor}px;
-            background: gray;
-            transition: .3s;
-        }
-        .ts-label-${options.id} {
-            font-size: ${factor * 0.5}px;
-            ${borderChar(factor * 0.5, 'black')};
-        }
-    </style>
-    <input type='checkbox' class='${options.id}' style='display: none'>        
-    <div class='in flr ts-label-${options.id}'>${options.label[0]}</div>
-    <div class='in flr ts-container-${options.id}'>
-        <div class='abs ts-round-${options.id}' style='top: ${factor * 0.2}px; left: ${factor * 0.2}px;'>
-        </div>
-    </div> 
-    `;
 };
 
 const getQueryParams = () => {
