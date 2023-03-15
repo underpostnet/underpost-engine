d8c21cd58d384262b6866c1c2d5b0ccf9a188f0a
fcoverdugo <fcoverdugoa@underpost.net>
2023-03-14 23:01:05 -0300

    update

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { emailValidator, passwordValidator, usernameValidator, passwordMatchValidator } from './common.js';
+import { emailValidator, passwordValidator, usernameValidator, passwordMatchValidator, renderLang } from './common.js';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
@@ -13,12 +13,17 @@ const getUsers = () => JSON.parse(fs.readFileSync(collectionPath, 'utf8'));
 const writeUsers = (users) => fs.writeFileSync(collectionPath, JSON.stringify(users, null, 4), 'utf8');
 if (!fs.existsSync(collectionFolder)) fs.mkdirSync(collectionFolder, { recursive: true });
 if (!fs.existsSync(collectionPath)) writeUsers([]);
+const saltRounds = 10;
 
-const register = (req, res) => {
+const register = async (req, res) => {
   try {
     console.log('register', req.body);
 
-    const { username, email, password, repeat_password } = req.body;
+    let { username, email, password, repeat_password } = req.body;
+
+    email = email.toLowerCase().trim();
+    username = username.trim();
+
     if (
       !emailValidator(email, req).validate ||
       !passwordValidator(password, req).validate ||
@@ -32,10 +37,50 @@ const register = (req, res) => {
         },
       });
 
+    if (getUsers().find((user) => user.email === email))
+      return res.status(400).json({
+        status: 'error',
+        data: {
+          message: renderLang({ es: 'email existente', en: 'email already exist' }, req),
+        },
+      });
+
+    if (getUsers().find((user) => user.username.toLowerCase() === username.toLowerCase()))
+      return res.status(400).json({
+        status: 'error',
+        data: {
+          message: renderLang({ es: 'username existente', en: 'username already exist' }, req),
+        },
+      });
+
+    password = await new Promise((resolve, reject) => {
+      try {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hash) => {
+            // Store hash in your password DB.
+            resolve(hash);
+          });
+        });
+      } catch (error) {
+        return res.status(500).json({
+          status: 'error',
+          data: {
+            message: error.message,
+          },
+        });
+      }
+    });
+
+    const user = { username, email, password };
+    const users = getUsers();
+    users.push(user);
+    writeUsers(users);
+
     return res.status(200).json({
       status: 'success',
       data: {
-        message: 'ok',
+        message: renderLang({ es: 'usuario creado', en: 'username created' }, req),
+        user,
       },
     });
   } catch (error) {
