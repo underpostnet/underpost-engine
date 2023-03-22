13f8e52de6b83a7948cee3d1e9d38a366992423a
fcoverdugo <fcoverdugoa@underpost.net>
2023-03-22 14:13:45 -0300

    update

--- src/cyberia/components/bag.js
+++ src/cyberia/components/bag.js
@@ -1,51 +1,43 @@
 const bag = () => {
+  const renderKoynLogo = (cryto) => /*html*/ `
+  
+       <div class='inl coin-value-content' style='${borderChar(2, 'black')}'>
+          <div class='fl'  style='height: 100%;'>
+              <div class='in fll' style='width: 50%; height: 100%;'>
+                <img class='abs center koyn-gif' src='/gifs/koyn.gif'>
+                <div class='abs center'>
+                  ${
+                    cryto
+                      ? /*html*/ `
+                  <span style='color: black; font-weight: bold; ${borderChar(1, 'white')}'>
+                    Crypto
+                  </span>
+                  <br>
+                  `
+                      : ''
+                  }
+                  Ko<span style='color: red;'>λ</span>n
+                </div>
+              </div>
+              <div class='in fll' style='width: 50%; height: 100%; background: #090909;'>
+                <div class='abs center value-koyn-text'>
+                    0
+                </div>
+              </div>
+          </div>
+      </div>
+  
+  `;
+
   return /*html*/ `
     <bag style='display: none'>
       <sub-content-gui class='in'>
 
-             ${renderLang({ es: 'Mochila', en: 'Bag' })}
+            ${renderLang({ es: 'Mochila', en: 'Bag' })}
             <hr>
-
-            <div class='inl coin-value-content' style='${borderChar(2, 'black')}'>
-                <div class='fl'  style='height: 100%;'>
-                    <div class='in fll' style='width: 50%; height: 100%;'>
-                      <img class='abs center koyn-gif' src='/gifs/koyn.gif'>
-                      <div class='abs center'>
-                        Ko<span style='color: red;'>λ</span>n
-                      </div>
-                    </div>
-                    <div class='in fll' style='width: 50%; height: 100%; background: #090909;'>
-                      <div class='abs center value-koyn-text'>
-                         0
-                      </div>
-                    </div>
-                </div>
-            </div>
-
+            ${renderKoynLogo()}
             <br>
-
-            <div class='inl coin-value-content' style='${borderChar(2, 'black')}'>
-                <div class='fl'  style='height: 100%;'>
-                    <div class='in fll' style='width: 50%; height: 100%;'>
-                      <img class='abs center koyn-gif' src='/gifs/koyn.gif'>
-                      <div class='abs center'>
-                        <span style='color: black; font-weight: bold; ${borderChar(1, 'white')}'>
-                          Crypto
-                        </span>
-                        <br>
-                        Ko<span style='color: red;'>λ</span>n
-                      </div>
-                    </div>
-                    <div class='in fll' style='width: 50%; height: 100%; background: #090909;'>
-                      <div class='abs center value-koyn-text'>
-                         0
-                      </div>
-                    </div>
-                </div>
-            </div>
-
-
-           
+            ${renderKoynLogo({})}          
 
       </sub-content-gui>
     </bag>
