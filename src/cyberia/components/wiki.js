a38cfa757aaf359b9e34500d71431b27d421603a
fcoverdugo <fcoverdugoa@underpost.net>
2023-04-26 13:53:15 -0400

    update

--- src/cyberia/components/wiki.js
+++ src/cyberia/components/wiki.js
@@ -1,4 +1,15 @@
 const wiki = () => {
+  const wikiMetaData = [
+    {
+      name: renderLang({ es: 'Controles', en: 'Controls' }),
+      open: true,
+      render: () => {
+        return /*html*/ `
+            test
+        `;
+      },
+    },
+  ];
   return /*html*/ `
     
     <wiki style='display: none'>
@@ -6,7 +17,33 @@ const wiki = () => {
 
             <div class='in title-section'>${renderLang({ es: 'Wiki', en: 'Wiki' })}</div>
             
-            
+            ${wikiMetaData
+              .map((wmd, i) => {
+                let openSection = false;
+                setTimeout(() => {
+                  s(`.wsh-${i}`).onclick = () => {
+                    if (openSection) {
+                      openSection = false;
+                      htmls(`.wsh-icon-${i}`, '>');
+                      s(`.ws-${i}`).style.display = 'none';
+                    } else {
+                      openSection = true;
+                      htmls(`.wsh-icon-${i}`, 'X');
+                      s(`.ws-${i}`).style.display = 'block';
+                    }
+                  };
+                  if (wmd.open) s(`.wsh-${i}`).click();
+                });
+                return /*html*/ `
+                <div class='in wiki-section-header wsh-${i} custom-cursor'>
+                    <span class='wsh-icon-${i}'>></span> ${wmd.name}
+                </div>
+                <div class='in wiki-section ws-${i}' style='display: none'>
+                    ${wmd.render()}
+                </div>
+                `;
+              })
+              .join('')}
 
         </sub-content-gui>
     </wiki>
