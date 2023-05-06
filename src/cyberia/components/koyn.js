0f097b11ea3131ebb176ab7862bbbfd5d09aca14
fcoverdugo <fcoverdugoa@underpost.net>
2023-05-06 18:25:38 -0400

    update

--- src/cyberia/components/koyn.js
+++ src/cyberia/components/koyn.js
@@ -1,28 +1,30 @@
 const renderKoynLogo = (value, type, valueID) => {
-  const name = type === 'crypto' ? 'Crypto Koyn' : 'Koyn';
+  const koynName = () => /*html*/ `
+  ${
+    type === 'crypto'
+      ? /*html*/ `
+  <span style='color: black; font-weight: bold; ${borderChar(1, 'white')}'>
+    Crypto
+  </span>
+  <br>
+  `
+      : ''
+  }
+  Ko<span style='color: red;'>λ</span>n
+  `;
   return {
     render: () => /*html*/ `
   
     <img class='abs center item-bag-icon' src='/gifs/koyn.gif'>
     <div class='abs center item-bag-style-text'>
-      ${
-        type === 'crypto'
-          ? /*html*/ `
-      <span style='color: black; font-weight: bold; ${borderChar(1, 'white')}'>
-        Crypto
-      </span>
-      <br>
-      `
-          : ''
-      }
-      Ko<span style='color: red;'>λ</span>n
+      ${koynName()}
     </div>
     ${renderItemCount(valueID, value)}
 
 `,
     data: {
       id: type === 'crypto' ? 'crypto-koyn' : 'koyn',
-      name: { es: name, en: name },
+      name: { es: koynName(), en: koynName() },
     },
   };
 };
